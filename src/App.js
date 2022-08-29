f882cc9621857b98f2605de9386d1c90d40dbb68
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-08-29 14:17:11 +0600

    navbar commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./app.css";
+import Home from "./pages/Home";
+import Header from "./components/header/Header";
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
