5e50f99b2111b43b2563be8cd210ba67833f9240
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-08-30 12:32:30 +0600

    create choose us components file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./app.css";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Header from "./components/header/Header";
 import { Fragment } from "react";
 import HeroSlider from "./components/hero-slider/HeroSlider";
---
+++ src/components/choose-us/ChooseUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ChooseUs.css';
+import { Container, Row, Col } from "reactstrap";
+
+const ChooseUs = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChooseUs;
